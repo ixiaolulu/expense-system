84deec7a0aedd533f2445dd28812e6f18a57deb2
dxl8913167@163.com <ding123456>
2017-10-29 17:37:36 +0800

    springmvc框架搭建

--- src/main/java/com/lulu/expense/controller/LiveController.java
+++ src/main/java/com/lulu/expense/controller/LiveController.java
@@ -2,6 +2,7 @@ package com.lulu.expense.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Author: Milo Ting
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @ModifiedBy:
  */
 @Controller
-@RequestMapping("/expense")
+@RequestMapping(value = "/expense",method = RequestMethod.GET)
 public class LiveController {
     @RequestMapping("/isLive")
     public String isLive(){
